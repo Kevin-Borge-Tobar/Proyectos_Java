04cae065658c15281817eb8ea8717756103044a6
Kevin Borge Tobar <kevinborgetobar1998@gmail.com>
2021-02-25 01:07:30 -0600

    Hola Mundo desde Java

---
+++ Hola Mundo desde Java/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+        System.out.println("Gracias");
+        System.out.println("Fin del programa");
+    }
+}
